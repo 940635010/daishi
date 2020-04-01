3dc207dcdbd9b8eda6ac9ef4880291e282533112
yang6701218 <648761695@qq.com>
2020-04-01 21:59:18 +0800

    新增

--- daishi-pc/js/index.js
+++ daishi-pc/js/index.js
@@ -4,6 +4,8 @@ $('.list-li').on('click',function(){
 	// console.log(index);
 	$(this).addClass("active").siblings().removeClass("active");
     $(".step-content").eq(index).addClass("show").siblings().removeClass("show");
+    $('.step-line').eq(index).addClass('flow').siblings().removeClass("flow");
+    console.log($('.step-line').eq(index))
 })
 
 // 戴氏资讯
@@ -14,4 +16,34 @@ $('.infor-list').on('click',function(){
     $(".l-text").eq(index).addClass("infor-show").siblings().removeClass("infor-show");
 })
 
+$('.xb-grow1').delegate('div','click',function(){
+    $(this).removeClass('xb-grow12')
+    $(this).addClass("xb-grow11")
+    $(this).siblings().removeClass('xb-grow11')
+    $(this).siblings().addClass("xb-grow12")
+})
+ $('#fifteen').on('click',function(){
+     $('#seeblue').css('display','block')
+     $('.xb-grow2').css('display','none')
+     $('.xb-grow3').css('display','none')
+     $('#anotherpic').css('display','none')
+     $('#innerbtn').css('display','none')
+     $('#innerbt').css('display','block')
+ }) 
+ $('#eleven').on('click',function(){
+     $('#seeblue').css('display','none')
+     $('.xb-grow2').css('display','flex')
+     $('.xb-grow3').css('display','flex')
+     $('#anotherpic').css('display','none')
+     $('#innerbtn').css('display','none')
+     $('#innerbt').css('display','none')
+ }) 
+ $('#twelve').on('click',function(){
+     $('#seeblue').css('display','none')
+     $('#anotherpic').css('display','block')
+     $('.xb-grow2').css('display','none')
+     $('.xb-grow3').css('display','none')
+     $('#innerbtn').css('display','block')
+     $('#innerbt').css('display','none')
+ })
 
