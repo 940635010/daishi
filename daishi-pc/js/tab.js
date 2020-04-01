087790922ffae58a81c7c440b8db11987b15784b
940635010 <940635010@qq.com>
2020-04-01 22:02:22 +0800

    pc和H5修改

---
+++ daishi-pc/js/tab.js
@@ -0,0 +1,15 @@
+$('.dianji').on('click',function(){
+    $('.xb-grow').addClass('quxiao')
+    $('.shang-tab').removeClass('quxiao')
+    $('.shang-tab1').addClass('quxiao')
+})
+$('.dianji1').on('click',function(){
+    $('.xb-grow').removeClass('quxiao')
+    $('.shang-tab').addClass('quxiao')
+    $('.shang-tab1').addClass('quxiao')
+})
+$('.dianji2').on('click',function(){
+    $('.xb-grow').addClass('quxiao')
+    $('.shang-tab').addClass('quxiao')
+    $('.shang-tab1').removeClass('quxiao')
+})
