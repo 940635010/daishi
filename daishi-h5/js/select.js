cd83b8330c9050ae061b95816a478d6897f46ec7
940635010 <940635010@qq.com>
2020-04-02 21:44:16 +0800

    BUG提交

--- daishi-h5/js/select.js
+++ daishi-h5/js/select.js
@@ -33,11 +33,13 @@
     }
     // 打开选项
     Select.prototype.click=function(){
+        console.log(1)
         if(this.position.css('overflow')=='hidden'){
-            this.position.css({'overflow':'visible','height':'100%'})
+            this.position.css({'overflow':'visible'})
         }else{
             this.position.css({'overflow':'hidden','height':this.hei})
         }
+        e.stopPropagation();
     }
     // 点击展开的元素
     Select.prototype.onchild=function(_this){
@@ -54,24 +56,24 @@
 // arr数组arr:["幼儿1","一年级","二年级","三年级","四年级","五年级","六年级","初一","初二","初三","高一","高二","高三"]
 // 要求的宽高width:'100px',height:'20px'
 // 传的是点击下拉的宽度drop:'200px'，超出的隐藏
-new Select({
-    position:$('.shangke-select1'),
-    arr:["年级","幼儿","一年级","二年级","三年级","四年级","五年级","六年级","初一","初二","初三","高一","高二","高三"],
-    width:'48%',
-    height:'35px',
-    drop:'180px'
-})
-new Select({
-    position:$('.shangke-select2'),
-    arr:["科目",'语文','数学','英语','物理','化学','语言表演','情商美术'],
-    width:'48%',
-    height:'35px',
-    drop:'180px'
-})
-new Select({
-    position:$('.shangke-select3'),
-    arr:["班级",'一对一','3~6人精品','15人大师班'],
-    width:'48%',
-    height:'35px',
-    drop:'180px'
-})
+// new Select({
+//     position:$('.shangke-select1'),
+//     arr:["年级","幼儿","一年级","二年级","三年级","四年级","五年级","六年级","初一","初二","初三","高一","高二","高三"],
+//     width:'48%',
+//     height:'35px',
+//     drop:'180px'
+// })
+// new Select({
+//     position:$('.shangke-select2'),
+//     arr:["科目",'语文','数学','英语','物理','化学','语言表演','情商美术'],
+//     width:'48%',
+//     height:'35px',
+//     drop:'180px'
+// })
+// new Select({
+//     position:$('.shangke-select3'),
+//     arr:["班级",'一对一','3~6人精品','15人大师班'],
+//     width:'48%',
+//     height:'35px',
+//     drop:'180px'
+// })
