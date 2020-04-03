c45ef388b6d14604fef46123d1cfcc7fbd9af99c
wusiyu <wusiyu@163.com>
2020-04-03 22:08:50 +0800

    n4-3

---
+++ daishi-h5/js/prev.js
@@ -0,0 +1,309 @@
+
+    let info =  [ 
+      //1
+      {
+        name:"张林敏",
+        msg:"善于寓教于乐，教学中注重让孩子的轻松，幽默的环境中学习和探索，并借鉴发达国家开放式教育模式...",
+        img:"",
+        sch_img:""
+      },
+      //2
+      {
+        name:"吴连秀",
+        msg:"提倡课堂效率和学习兴趣的提高，使学生得到启发从而好奇，因为好奇从而思考，在亦学亦思的学习氛围中获得...",
+        img:"",
+        sch_img:""
+      },
+      //3
+      {
+        name:"唐俐",
+        msg:"教学目标明确，教学方法灵活多变，善于启发学生学习，让学生学习能力得到提高。 同时又注重学生学习兴趣...",
+        img:"",
+        sch_img:""
+      },
+      //4
+      {
+        name:"邱露莹",
+        msg:"善于根据孩子的年龄特点，设置动感课堂，以培养孩子听、说、读、写的能力为主，运用游戏教学创设情景...",
+        img:"",
+        sch_img:""
+      },
+      //5
+      {
+        name:"庞红梅",
+        msg:"教学形式活泼，教学内容丰富。注重培养学生的综合素质和自主学习能力。寓教于乐，通过课堂教学来激发学生的...",
+        img:"",
+        sch_img:""
+      },
+      //6
+      {
+        name:"胡灵芝",
+        msg:"思考与实践的课堂，游戏与唱跳的融入，采取独特的“讨论式”教学方式。坚信英语学习需要听、说、读、写坚持...",
+        img:"",
+        sch_img:""
+      },
+      //7
+      {
+        name:"袁明超",
+        msg:"围绕“有效，有趣，乐学，好学”开展教学活动，以学生为主体，在活跃愉快的课堂氛围中诱发学生的学习乐趣...",
+        img:"",
+        sch_img:""
+      },
+      //8
+      {
+        name:"杨智",
+        msg:"热爱教育，风趣幽默的教学方式改变枯燥的数学形象，丰富的教学经验让课堂更加精准有效，快速提分重塑学...",
+        img:"",
+        sch_img:""
+      },
+      //9
+      {
+        name:"罗骋骁",
+        msg:"教学形式多样化，课堂妙趣横生，幽默而不失严谨。 注重数学与生活，数学与其它学科之间的联系...",
+        img:"",
+        sch_img:""
+      },
+      //10
+      {
+        name:"邹玓娟",
+        msg:"对课堂全身心的投入，课堂感染力强，教学中做到详略得当，循循善诱，提倡课堂学习以学生为主体...",
+        img:"",
+        sch_img:""
+      },
+      //11
+      {
+        name:"罗锐",
+        msg:"教学方法灵活多样，能根据学生的实际情况恰当地选择教学方法。 思路清晰，因材施教...",
+        img:"",
+        sch_img:""
+      },
+      //12
+      {
+        name:"胡巧",
+        msg:"课堂幽默风趣，教学风格独树一帜，知识分析深入浅出，善于运用启发式教学调动学生主观能动性...",
+        img:"",
+        sch_img:""
+      },
+      //13
+      {
+        name:"许小丽",
+        msg:"关注学生，注重学生评价。 激发学生的兴趣。 教学方法灵活，根据教学难度的高低与学生的接受能力恰当地选择...",
+        img:"",
+        sch_img:""
+      },
+      //14
+      {
+        name:"李飞甫",
+        msg:"极其高的责任心，真切关心和心灵引导促进学生心理成长，在教学中，采用灵活多样的教学方法...",
+        img:"",
+        sch_img:""
+      },
+      //15
+      {
+        name:"张欢",
+        msg:"对课堂全身心的投入，富有激情，课堂感染力强，教学中做到详略得当，循循善诱，提倡课堂学习以学生为主体...",
+        img:"",
+        sch_img:""
+      },
+      //16
+      {
+        name:"魏泽",
+        msg:"连续从事初毕业班数学教学多年，对高考数学具有丰富的教学经验，提倡“分层教学法”：重技巧，重方法...",
+        img:"",
+        sch_img:""
+      },
+      //17
+      {
+        name:"徐营营",
+        msg:"教学方法灵活多样，能根据学生的实际情况恰当地选择教学方法。 思路清晰，因材施教。 课堂气氛轻松活跃...",
+        img:"",
+        sch_img:""
+      },
+      //18
+      {
+        name:"张萌",
+        msg:"根据孩子的生理及心理特点，以创意美术为教学理念，以快乐美术为教学方法，在教学中以培养审美创造为核心...",
+        img:"",
+        sch_img:""
+      },
+      //19
+      {
+        name:"覃怡",
+        msg:"以创意美术为教学理念，以快乐美术为教学方法，在教学中以培养审美创造为核心，鼓励孩子的创造性想法和表达...",
+        img:"",
+        sch_img:""
+      },
+      //20
+      {
+        name:"蒋林艳",
+        msg:"用心传递每一份关爱，蒋老师性格开朗活泼，上课幽默风趣！对孩子家庭教育方面有独特的教育方式...",
+        img:"",
+        sch_img:""
+      },
+      //21
+      {
+        name:"何婉如",
+        msg:"崇尚以爱和耐心感化孩子，走进孩子的世界，课堂教学生动活泼，为孩子营造宽松、愉悦的学习氛围...",
+        img:"",
+        sch_img:""
+      },
+      //22
+      {
+        name:"张林君",
+        msg:"采用“三心课堂”——以“爱心、耐心、细心”作为教学宗旨。“二分教学”——擅长因势利导，分类指导...",
+        img:"",
+        sch_img:""
+      },
+      //23
+      {
+        name:"黄群焯",
+        msg:"课堂教学生动活泼，善于用有趣的语言结合教学内容授课，最大的调动学生的学习积极性，为孩子营造宽松...",
+        img:"",
+        sch_img:""
+      },
+      //24
+      {
+        name:"尹翔翀",
+        msg:"教学形式多样化，课堂妙趣横生，幽默而不失严谨。 注重数学与生活，数学与其它学科之间的联系...",
+        img:"",
+        sch_img:""
+      },
+      //25
+      {
+        name:"王玉赛",
+        msg:"教学方法灵活多样，能根据学生的实际情况恰当地选择教学方法。 思路清晰，因材施教。 课堂气氛轻松活跃...",
+        img:"",
+        sch_img:""
+      },
+      //26
+      {
+        name:"李奇",
+        msg:"在公立学校从事高三毕业班生物教书工作，后进入戴氏教学部，从事工作以来，崇尚以爱和耐心感化孩子...",
+        img:"",
+        sch_img:""
+      },
+      //27
+      {
+        name:"刘碧君",
+        msg:"关注学生，注重学生评价。 激发学生的兴趣。 教学方法灵活，根据教学难度的高低与学生的接受能力恰当地选择...",
+        img:"",
+        sch_img:""
+      },
+      //28
+      {
+        name:"范相君",
+        msg:"对课堂全身心的投入，富有激情，课堂感染力强，教学中做到详略得当，循循善诱，提倡课堂学习以学生为主体...",
+        img:"",
+        sch_img:""
+      },
+      //29
+      {
+        name:"林涵",
+        msg:"极其高的责任心，真切关心和心灵引导促进学生心理成长，在教学中，采用灵活多样的教学方法，充分利用观察...",
+        img:"",
+        sch_img:""
+      },
+      //30
+      {
+        name:"唐华",
+        msg:"数学系高材生。连续从事高三毕业班数学教学多年，对高考数学具有丰富的教学经验，提倡“分层教学法”：重技...",
+        img:"",
+        sch_img:""
+      },
+      {
+        name:"顾金刚",
+        msg:"睿智而博学，精通汉语文化，清华，北大，复旦，南开，浙大弟子百数。顾老师因材施教，富有创意...",
+        img:"",
+        sch_img:""
+      }
+      //31
+    ];
+    let _top = document.getElementById("_top");
+
+    let head = document.getElementsByClassName("h_img")[0];
+    let arr = [];
+    for(let i=1;i<32;i++){
+      let div = document.createElement("div");
+      let son_1 = document.createElement("div");
+      let img = document.createElement("img");
+      div.setAttribute('class','swiper-slide _head');
+      son_1.setAttribute('class',`s_head  ${'son'+i}`);
+      img.setAttribute('src',`../images/img/b_head/${i}.jpg`);
+      info[i-1].img = `../images/img/b_head/${i}.jpg`;
+      info[i - 1].sch_img = `../images/img/school/${i}.png`;
+      son_1.appendChild(img);
+      div.appendChild(son_1);
+      arr.push(div);
+    }
+    for(let value of arr){
+      head.appendChild(value);
+    }
+    head.children[0].classList.add('opc');
+   
+    $(".s_head").click(function(e){
+      for(let i of head.children){
+        i.className.indexOf('opc') != -1
+        ?i.classList.remove('opc')
+        :null;
+      }
+      e.target.parentNode.parentNode.classList.add('opc');
+    });
+
+    let newArr = [];
+    for(let obj of info ){
+      let div = document.createElement("div");
+      let father = document.createElement("div"); // son_1,son_2,img, 为father的儿子
+      let son_1 = document.createElement("div"); // 对应的名字
+      let son_2 = document.createElement("div"); // 对应的info info_cont 是info的儿子
+      // let info_cont = document.createElement("div");
+      let info_cont = document.createElement("div");// cont_img,cont_span,cont_div 是info_cont的儿子
+      let img = document.createElement("img");
+      let cont_img = document.createElement("img");
+      let cont_span = document.createElement("span");
+      let cont_div = document.createElement("div");
+      son_1.innerText = obj.name;
+      cont_span.innerText = obj.msg;
+      cont_div.innerText = '与ta交流 > ';
+      img.setAttribute('src',`${obj.img}`);
+      cont_img.setAttribute('src', `${obj.sch_img}`);
+      cont_div.setAttribute('src',`cont_div`);
+      img.setAttribute('class',"b_head");
+      son_1.setAttribute('class','_name');
+      son_2.setAttribute('class','info');
+      info_cont.setAttribute('class','info_cont');
+      info_cont.appendChild(cont_img);
+      info_cont.appendChild(cont_span);
+      info_cont.appendChild(cont_div);
+      son_2.appendChild(info_cont);
+      father.appendChild(img);
+      father.appendChild(son_1);
+      father.appendChild(son_2);
+      div.setAttribute('class','swiper-slide swiper-no-swiping');
+      father.setAttribute('class','father');
+      div.appendChild(father);
+      newArr.push(div);
+    }
+      for(let val of newArr){
+        _top.appendChild(val)
+      }
+
+
+    var thumbsSwiper = new Swiper('#thumbs',{
+    spaceBetween: 10,
+    slidesPerView: 4,
+    loop:true,
+    watchSlidesVisibility: true,//防止不可点击
+    })
+
+    var gallerySwiper = new Swiper('#gallery',{
+      spaceBetween: 10,
+      loop: true,
+      // autoplay: {
+      // delay: 1000,
+      // stopOnLastSlide: false,
+      // disableOnInteraction: true,
+      // },
+      thumbs: {
+      swiper: thumbsSwiper,
+      }
+    })
